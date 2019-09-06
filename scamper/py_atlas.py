401d0607122bd56c45d5e63e29a61040b1f7ca08
amarder <amarder@seas.upenn.edu>
2019-09-06 11:45:12 -0700

    weird atlas format

--- scamper/py_atlas.py
+++ scamper/py_atlas.py
@@ -1,4 +1,6 @@
 import json
+from socket import AF_INET6
+from socket import AF_INET
 from typing import List, Any, Dict
 
 from jsonschema import validate
@@ -12,86 +14,75 @@ class AtlasErrorException(Exception):
 
 
 class AtlasHop(Hop):
-    def __init__(self, hop=-1, rtt=float('nan'), size=-1, ttl=-1, err=None, itos=0, ittl=1, flags=None, icmpext=None, late=0, dup=0, edst=None, hdropts=None, **kwargs):
-        if err is None:
-            icmp_type = 11
-            icmp_code = 0
+    def __init__(self, hop=-1, rtt=float('nan'), size=-1, ttl=1, err=None, itos=0, ittl=1, family=0, **kwargs):
+        if not err:
+            if family == AF_INET:
+                icmp_type = 11
+                icmp_code = 0
+            else:
+                icmp_type = 3
+                icmp_code = 0
         else:
-            icmp_type = 3
+            icmp_type = 3 if family == AF_INET else 1
             if isinstance(err, str):
                 if err == 'N':
-                    icmp_code = 0
-                elif err == 'H':
-                    icmp_code = 1
+                    icmp_code = 0 if family == AF_INET else 0
+                elif err == 'H' or 'h':
+                    icmp_code = 1 if family == AF_INET else 3
                 elif err == 'A':
-                    icmp_code = 13
+                    icmp_code = 13 if family == AF_INET else 5
                 elif err == 'P':
-                    icmp_code = 2
+                    icmp_code = 2 if family == AF_INET else 4
                 elif err == 'p':
-                    icmp_code = 3
+                    icmp_code = 3 if family == AF_INET else 4
+                # elif err == 'h':
+                #     raise AtlasErrorException('No clue what h means')
                 else:
-                    raise AtlasErrorException('Unknown error code: {}'.format(err))
-            elif isinstance(err, int):
-                icmp_code = err
+                    icmp_code = int(err)
+                    # raise AtlasErrorException('Unknown error code: {}'.format(err))
             else:
-                raise AtlasErrorException('Unknown error code: {}'.format(err))
-        self.addr = kwargs.get('from')
-        self.probe_ttl = hop
-        self.rtt = rtt
-        self.reply_ttl = ttl
-        self.reply_size = size
-        self.icmp_type = icmp_type
-        self.icmp_code = icmp_code
-        self.icmpext = icmpext
-        self.ismpls = bool(icmpext)
-        self.reply_tos = itos
-        self.icmp_q_ttl = ittl
-
-        self.flags = flags
-        self.late = late
-        self.dup = dup
-        self.edst = edst
-        self.hdropts = hdropts
+                icmp_code = err
+        addr = kwargs.get('from')
+        probe_ttl = hop
+        rtt = rtt
+        reply_ttl = ttl
+        reply_size = size
+        icmp_type = icmp_type
+        icmp_code = icmp_code
+        reply_tos = itos
+        icmp_q_ttl = ittl
+        super().__init__(addr, probe_ttl, rtt, reply_ttl, reply_tos, reply_size, icmp_type, icmp_code, icmp_q_ttl, -1, -1)
+        self.params = kwargs
 
 
 class AtlasTrace(Trace):
-    def __init__(self, af=0, dst_addr='', dst_name='', endtime=0, fw=0, group_id=0, lts=0, msm_id=0, msm_name='', paris_id=0, prb_id=0, proto='', result=None, size=0, src_addr='', timestamp=0, type='', **kwargs):
-        print('in AtlasTrace: {}'.format(result))
-        super().__init__(src_addr, dst_addr, create_hops(result))
-        # self.src = src_addr
-        # self.dst = dst_addr
-        # self.hops = create_hops(result)
-
-        self.af = af
-        self.dst_addr = dst_addr
-        self.dst_name = dst_name
-        self.endtime = endtime
-        self.fw = fw
-        self.group_id = group_id
-        self.lts = lts
-        self.msm_id = msm_id
-        self.msm_name = msm_name
-        self.paris_id = paris_id
-        self.prb_id = prb_id
-        self.proto = proto
-        self.result = result
-        self.size = size
-        self.src_addr = src_addr
-        self.timestamp = timestamp
-        self.type = type
+    def __init__(self, dstAddress='', IpFrom='', af='4', hops=None, **kwargs):
+        af = int(af)
+        if af == 4:
+            family = AF_INET
+        else:
+            family = AF_INET6
+        super().__init__(IpFrom, dstAddress, create_hops(hops, family))
+        self.family = family
+        self.params = kwargs
 
 
-def create_hops(hops: List[Dict[str, Any]]):
+def create_hops(hops: List[Dict[str, Any]], family):
     hopslist = []
     if hops is not None:
         for i in range(len(hops)):
             h = hops[i]
-            results = h.get('result')
+            results = h.get('resultHops')
             if not results:
                 continue
-            for result in h['result']:
+            probe_ttl = int(h['hop'])
+            for result in h['resultHops']:
                 if 'from' in result:
-                    hop = AtlasHop(hop=h['hop'], **result)
+                    try:
+                        hop = AtlasHop(hop=probe_ttl, family=family, **result)
+                    except:
+                        print(result)
+                        raise
                     hopslist.append(hop)
                     break
     return hopslist
@@ -108,18 +99,10 @@ class AtlasReader(Reader):
     def __iter__(self):
         for line in self.f:
             j = json.loads(line)
-            # print(j)
-            if isinstance(j, list):
-                for result in j:
-                    if not 'type' in result or result['type'] == 'traceroute':
-                        validate(j, self.schema)
-                        yield AtlasTrace(**result)
-            else:
-                result = j
-                if not 'type' in result or result['type'] == 'traceroute':
-                    print('Validating')
-                    validate(j, self.schema)
-                    yield AtlasTrace(**result)
+            result = j
+            if 'type' not in result or result['type'] == 'traceroute':
+                # validate(j, self.schema)
+                yield AtlasTrace(**result)
 
     def open(self):
         self.f = File2(self.filename)
