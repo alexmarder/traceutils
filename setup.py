748fe70d7ff83dded5afab5fdae79197b63ad98b
amarder <amarder@seas.upenn.edu>
2019-11-04 14:06:01 -0800

    release 6.15.2

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ package_data = {k: ['*.pxd'] for k in extensions_names}
 
 setup(
     name="traceutils",
-    version='6.15.1',
+    version='6.15.2',
     author='Alex Marder',
     description="Various packages for traceroute and BGP dump analysis.",
     url="https://github.com/alexmarder/traceutils",
