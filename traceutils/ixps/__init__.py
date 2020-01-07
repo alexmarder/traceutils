19632867bfc9585ef9964f90c83f24a4c08d94c8
Alex Marder <amarder89@gmail.com>
2020-01-07 10:53:24 -0800

    scamper stuff

--- traceutils/ixps/__init__.py
+++ traceutils/ixps/__init__.py
@@ -1,1 +1,12 @@
+from traceutils.ixps.peeringdb_base import AbstractPeeringDB
+from traceutils.ixps import peeringdb, peeringdb_v1
 from traceutils.ixps.ixps import *
+
+
+def create_peeringdb(filename):
+    if filename.endswith('.json'):
+        return peeringdb.PeeringDB(filename)
+    try:
+        return peeringdb.PeeringDB(filename)
+    except:
+        return peeringdb_v1.PeeringDB(filename)
--- traceutils/scamper/hop.pxd
+++ traceutils/scamper/hop.pxd
@@ -41,7 +41,7 @@ cdef class Trace:
 
     cpdef list addrs(self);
     cpdef void prune_dups(self) except *;
-    cpdef void prune_loops(self) except *;
+    cpdef void prune_loops(self, bint keepfirst=*) except *;
     cpdef void prune_private(self, IP2AS ip2as) except *
     cpdef void set_packed(self) except *;
 
